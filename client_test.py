3974f83303580d5ba3882c94938988f78a2a3770
ahmedqamesh <ahmed.qamesh@yahoo.com>
2019-11-22 13:48:42 +0100

    OPCUA images

--- client_test.py
+++ client_test.py
@@ -1,20 +1,24 @@
 from opcua import Client
 import time
+
+# define the OPCUA address
 url ="opc.tcp://localhost:4840/"
 client=Client(url)
 client.connect()
-print("Client connected")
+print('The OPCUA Client connected')    
 
 while True:
     get_ADCTRIM = client.get_node("ns=2;i=8476")
     ADCTRIM = get_ADCTRIM.get_value()
-    print("ADCTRIM:",ADCTRIM)
     
     get_NodeID = client.get_node("ns=2;i=8530")
     NodeID =get_NodeID.get_value()
-    print("NodeID:",NodeID)
     
     get_Status = client.get_node("ns=2;i=10395")
     Status = get_Status.get_value()
-    print ("Status:",Status)
-    time.sleep(1)
+    
+    
+    Info = "Time: %s | Status: %s | NodeID: %i |  ADCTRIM: %i" %(time.ctime(), Status, NodeID, ADCTRIM)
+    print(Info)
+    
+    time.sleep(2)
