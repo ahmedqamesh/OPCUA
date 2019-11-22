6cf3c5f7bca6c7d16384e83cb94bae618ba25379
ahmedqamesh <ahmed.qamesh@yahoo.com>
2019-11-22 13:02:55 +0100

    nothing

--- client_test.py
+++ client_test.py
@@ -9,10 +9,11 @@ while True:
     get_ADCTRIM = client.get_node("ns=2;i=8476")
     ADCTRIM = get_ADCTRIM.get_value()
     print("ADCTRIM:",ADCTRIM)
+    
     get_NodeID = client.get_node("ns=2;i=8530")
-    print(get_NodeID)
     NodeID =get_NodeID.get_value()
     print("NodeID:",NodeID)
+    
     get_Status = client.get_node("ns=2;i=10395")
     Status = get_Status.get_value()
     print ("Status:",Status)
